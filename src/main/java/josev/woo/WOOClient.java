3a2656625a13f5b2dae433d2a20719c9ac339426
josevictorbdbr <josevictorbdbr@gmail.com>
2026-03-10 13:29:41 -0300

    Initial commit

---
+++ src/main/java/josev/woo/WOOClient.java
@@ -0,0 +1,10 @@
+package josev.woo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WOOClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/josev/woo/WorldOfOres.java
@@ -0,0 +1,16 @@
+package josev.woo;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorldOfOres implements ModInitializer {
+	public static final String MOD_ID = "world-of-ores";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
