a97ab556e91101352ee57aab1d93b63225bc993a
josevictorbdbr <josevictorbdbr@gmail.com>
2026-03-10 13:56:14 -0300

    Added First item and setup for future addictions

---
+++ src/main/java/josev/woo/item/ModItems.java
@@ -0,0 +1,27 @@
+package josev.woo.item;
+
+import josev.woo.WorldOfOres;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(WorldOfOres.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        WorldOfOres.LOGGER.info("Registering ModItems" + WorldOfOres.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(ModItems.RUBY);
+        });
+    }
+
+}
